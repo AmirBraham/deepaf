9ca8d3ac9a4dae1b2999b97db971854f6781f8c3
Ronan Le Prat <ronan.leprat@telecom-paris.fr>
2023-06-21 14:45:53 +0200

    V1 model_single_frame

--- preprocessing/create_csv.py
+++ preprocessing/create_csv.py
@@ -3,18 +3,24 @@ import pandas as pd
 from os import listdir
 from os.path import isfile, join
 import random as rd
-fichiers1 = [f for f in listdir("./vox/test") if isfile(join("./vox/test", f))]
-fichiers2 = [f for f in listdir("./vox/train") if isfile(join("./vox/train", f))]
+fichiers1 = [f for f in listdir("./dataset-real/test") if isfile(join("./test", f))]
+fichiers2 = [f for f in listdir("./dataset-real/train") if isfile(join("./train", f))]
+fichiers3 = [f for f in listdir("./dataset-fake") if isfile(join("./test", f))]
 with open('dataset.csv', 'w') as f:
   writer = csv.writer(f)
   writer.writerow(["file_name", "label","status"])
   for fichier in fichiers1:
-  	if (rd.random() < 0.2):
-  		writer.writerow([fichier, "1", "test"])
-  	else:
-  		writer.writerow([fichier, "1", "train"])
+    if (rd.random() < 0.2):
+      writer.writerow([fichier, "1", "test"])
+    else:
+      writer.writerow([fichier, "1", "train"])
   for fichier in fichiers2:
-  	if (rd.random() < 0.2):
-  		writer.writerow([fichier, "1", "test"])
-  	else:
-  		writer.writerow([fichier, "1", "train"])
+    if (rd.random() < 0.2):
+      writer.writerow([fichier, "1", "test"])
+    else:
+      writer.writerow([fichier, "1", "train"])
+  for fichier in fichier3:
+    if (rd.random() < 0.2):
+      writer.writerow([fichier, "0", "test"])
+    else:
+      writer.writerow([fichier, "0", "train"])
--- preprocessing/model_single_frame.py
+++ preprocessing/model_single_frame.py
@@ -54,7 +54,7 @@ class CustomImageDataset(Dataset):
         frame = video_frames[frame_index]
 
         # Convertir la frame en objet PIL
-        frame_pil = Image.fromarray(frame.permute(1, 2, 0).numpy().astype('uint8'))
+        frame_pil = Image.fromarray(frame[0].numpy().astype('uint8')).convert('RGB')
 
         # Appliquer les transformations d'images à la frame
         if self.transform:
@@ -75,8 +75,81 @@ class CustomImageDataset(Dataset):
             frame_indices.append(random.randint(0, min_frames_per_video))  # Choix aléatoire d'une frame
         return frame_indices
 
-image_dataset_train = CustomImageDataset(annotations_file = "dataset.csv", video_dir = data_dir, status = "train", total_number=300)
-image_dataset_test = CustomImageDataset(annotations_file = "dataset.csv", video_dir = data_dir, status = "test", total_number = 300)
+image_dataset_train = CustomImageDataset(annotations_file = "dataset.csv", video_dir = data_dir, status = "train", total_number=500)
+image_dataset_test = CustomImageDataset(annotations_file = "dataset.csv", video_dir = data_dir, status = "test", total_number = 100)
 image_dataloader_train = DataLoader(image_dataset_train,batch_size=4,shuffle=True, num_workers=4)
 image_dataloader_test = DataLoader(image_dataset_test,batch_size=4,shuffle=True, num_workers=4)
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+dict = {"train" : image_dataloader_train, "test" : image_dataloader_test}
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    # Create a temporary directory to save training checkpoints
+    with TemporaryDirectory() as tempdir:
+        best_model_params_path = os.path.join(tempdir, 'best_model_params.pt')
+
+        torch.save(model.state_dict(), best_model_params_path)
+        best_acc = 0.0
+
+        for epoch in range(num_epochs):
+            print(f'Epoch {epoch}/{num_epochs - 1}')
+            print('-' * 10)
+
+            # Each epoch has a training and validation phase
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    model.train()  # Set model to training mode
+                else:
+                    model.eval()   # Set model to evaluate mode
+
+                running_loss = 0.0
+                running_corrects = 0
+
+                # Iterate over data.
+                for inputs, labels in dataloaders[phase]:
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
+
+                    # forward
+                    # track history if only in train
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
+
+                        # backward + optimize only if in training phase
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+
+                    # statistics
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+                if phase == 'train':
+                    scheduler.step()
+
+                epoch_loss = running_loss / dataset_sizes[phase]
+                epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+                # deep copy the model
+                if phase == 'val' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    torch.save(model.state_dict(), best_model_params_path)
+
+            print()
+
+        time_elapsed = time.time() - since
+        print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+        print(f'Best val Acc: {best_acc:4f}')
+
+        # load best model weights
+        model.load_state_dict(torch.load(best_model_params_path))
+    return model
