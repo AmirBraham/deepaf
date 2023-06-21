4f6af6c3c2c5069a171fd46b80cd56cb7b41b1cc
Planeur <philippegratias@orange.fr>
2023-06-21 11:22:20 +0200

    d

--- preprocessing/generate_deepfake.py
+++ preprocessing/generate_deepfake.py
@@ -6,8 +6,10 @@ FOLDER = "/home/pafvideo/deepaf/dataset-real"
 NB_DEEPFAKES = 10
 
 faces = listdir(join(FOLDER,"faces"))
+faces.shuffle()
 videos = listdir(join(FOLDER,"train"))
+videos.shuffle()
 for i in range(NB_DEEPFAKES):
-    random_face = join(join(FOLDER,"faces"),random.choice(faces))
-    random_video = join(join(FOLDER,"train"),random.choice(videos))
+    random_face = join(join(FOLDER,"faces"),faces[i])
+    random_video = join(join(FOLDER,"train"),videos[i])
     print(random_face, random_video)
