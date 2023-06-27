06c52aebf562fe5bc6d7b4a74a6ebba3d955a774
Amir <amirbrahamm@gmail.com>
2023-06-27 14:36:43 +0200

    New model taking the mean into account

--- test_evaluate/test_evaluate_TFP.py
+++ test_evaluate/test_evaluate_TFP.py
@@ -14,7 +14,17 @@ import math
 import pandas as pd
 import warnings
 import shutil
+import wandb
 
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+# Ignorer les avertissements
+warnings.filterwarnings("ignore")
+
+MIN_FRAMES_PER_VIDEO = 50
+NUMBER_FRAMES = 5
+
+data_dir = '.'
 
 data_transforms = {
     'train': transforms.Compose([
@@ -29,17 +39,16 @@ data_transforms = {
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
+        'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
 }
 
-# Ignorer les avertissements
-warnings.filterwarnings("ignore")
-
-MIN_FRAMES_PER_VIDEO = 50
-
-data_dir = '.'
-
 class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, video_dir, status="train", total_number=math.inf, transform=data_transforms, target_transform=None):
+    def __init__(self, annotations_file, video_dir, status="train", total_number=math.inf, number_frames = 1,transform=data_transforms, target_transform=None):
         df = pd.read_csv(annotations_file)
         self.video_labels = df[df["status"] == status]
         video_labels_0 = self.video_labels[self.video_labels["label"] == 0]
@@ -51,11 +60,10 @@ class CustomImageDataset(Dataset):
         video_labels_1 = video_labels_1.sample(num_samples)
 
         self.video_labels = pd.concat([video_labels_0, video_labels_1], ignore_index=True)[:total_number]
-        #self.video_labels = self.video_labels.sample(frac=1).reset_index(drop=True)[:min(len(self.video_labels), total_number)]
         self.video_dir = video_dir
         self.transform = transform[status]
         self.target_transform = target_transform
-        self.frame_indices = self._get_random_frame_indices(len(self.video_labels))
+        self.number_frames = number_frames
         print(self.video_labels)
 
     def __len__(self):
@@ -64,43 +72,40 @@ class CustomImageDataset(Dataset):
     def __getitem__(self, idx):
         l_status = self.video_labels.iloc[idx,2]
         l_fake = self.video_labels.iloc[idx,1]
+        subfolder = self.video_labels.iloc[idx,3]
+        file_name = self.video_labels.iloc[idx, 0]
         if (l_fake == "0"):
-            video_path = os.path.join(self.video_dir, self.video_labels.iloc[idx,3], self.video_labels.iloc[idx, 0])
+            video_path = os.path.join(self.video_dir, subfolder, file_name)
         else:
-            video_path = os.path.join(self.video_dir, self.video_labels.iloc[idx, 3], self.video_labels.iloc[idx, 0])
+            video_path = os.path.join(self.video_dir, subfolder, file_name)
 
         video_frames,_,_ = read_video(video_path)
-        frame_index = self.frame_indices[idx]
-        frame = video_frames[frame_index]
-        frame.size()
+        num_frames = len(video_frames)
 
-        # Convertir la frame en objet PIL
-        frame_pil = Image.fromarray(frame.numpy().astype('uint8')).convert('RGB')
-        frame_pil.save("working_frame.png")
+        frame_index = random.randint(0,num_frames-1-self.number_frames)
+        frames = [video_frames[frame_index + i] for i in range(self.number_frames)]
 
-        # Appliquer les transformations d'images à la frame
-        if self.transform:
-            frame_transformed = self.transform(frame_pil)
-        else:
-            frame_transformed = frame_pil
+        frames_transformed = []
+        for frame in frames:
+            # Convertir la frame en objet PIL
+            frame_pil = Image.fromarray(frame.numpy().astype('uint8')).convert('RGB')
+            frame_pil.save("working_frame.png")
+
+            # Appliquer les transformations d'images à la frame
+            if self.transform:
+                frames_transformed.append(self.transform(frame_pil))
+            else:
+                frames_transformed.append(frame_pil)
 
         label = self.video_labels.iloc[idx, 1]
-        subfolder = self.video_labels.iloc[idx,3]
-        file_name = self.video_labels.iloc[idx,0]
 
         if self.target_transform:
             label = self.target_transform(label)
 
-        return frame_transformed, label, (subfolder + "/" + file_name)
+        return frames_transformed, label, (subfolder + "/" + file_name)
 
-    def _get_random_frame_indices(self, num_videos):
-        frame_indices = []
-        for _ in range(num_videos):
-            frame_indices.append(random.randint(0, MIN_FRAMES_PER_VIDEO))  # Choix aléatoire d'une frame
-        return frame_indices
-
-image_dataset_test = CustomImageDataset(annotations_file = "dataset.csv", video_dir = data_dir, status = "val",total_number=2000)
-dataloaders = DataLoader(image_dataset_test,batch_size=1,shuffle=True, num_workers=4)
+image_dataset_test = CustomImageDataset(annotations_file = "dataset.csv", video_dir = data_dir, status = "val",total_number=1400,number_frames=NUMBER_FRAMES)
+dataloaders = DataLoader(image_dataset_test,batch_size=1,shuffle=True, num_workers=2)
 
 
 def visualize_model_predictions(model, dataloader):
@@ -115,13 +120,26 @@ def visualize_model_predictions(model, dataloader):
 
     with torch.no_grad():
         for inputs, labels, file_path in dataloader:
-            inputs = inputs.to(device)
+            inputs = inputs
             labels = labels.to(device)
 
-            outputs = model(inputs)
-            _, predictions = torch.max(outputs, 1)
+            num_frames = len(inputs)
+            batch_size = inputs[0].size(0)
 
-            for prediction, label in zip(predictions, labels):
+            temp_loss = 0
+            temp_preds = torch.Tensor([0 for i in range(batch_size)]).to(device)
+
+            for index in range(num_frames):
+                inputs_ieme_frame = inputs[index]
+                inputs_ieme_frame = inputs_ieme_frame.to(device)
+                outputs = model(inputs_ieme_frame)
+                _, preds = torch.max(outputs, 1)
+                temp_preds = torch.add(temp_preds,preds)
+
+            preds = torch.round(temp_preds/num_frames)
+            print(preds)
+
+            for prediction, label in zip(preds, labels):
                 if prediction == 1 and label == 1:
                     true_positives.append((inputs, prediction,file_path))
                 elif prediction == 1 and label == 0:
@@ -136,7 +154,7 @@ def visualize_model_predictions(model, dataloader):
 
     return true_positives, false_positives, true_negatives, false_negatives, total
 
-model_path = 'model.pt'
+model_path = 'model_5_frames_backward.pt'
 model = torchvision.models.resnet18(pretrained=False)
 num_ftrs = model.fc.in_features
 model.fc = nn.Linear(num_ftrs, 2)
@@ -164,12 +182,12 @@ def imsave(inp, file_path, title=None):
     plt.pause(0.001)  # pause a bit so that plots are updated
 
 for input, prediction, file_path in false_positives:
-    print(file_path)
+    print("FALSE POSITIVE:", file_path)
     destination_folder = "test_evaluate/false_positives"
     shutil.copy(file_path[0], destination_folder)
 
 for input, prediction, file_path in false_negatives:
-    print(file_path)
+    print("FALSE NEGATIVE",file_path)
     destination_folder = "test_evaluate/false_negatives"
     shutil.copy(file_path[0], destination_folder)
 
